0b9d813f4e9774f541288d461d413542cbf6d29a
Kevin <kn6267658@gmail.com>
2025-10-02 15:52:43 +0700

    ~ PlayerScript, MainSene

--- BridgeRace/Assets/Scripts/Player.cs
+++ BridgeRace/Assets/Scripts/Player.cs
@@ -2,36 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] private GameObject playerBrickPrefabs;
-    [SerializeField] private Transform tfPlayerModel;
-    [SerializeField] private Transform tfPlayerBrick;
+    [SerializeField] private Rigidbody rbPlayer;
+    [SerializeField] private FixedJoystick fJoyStick;
     [SerializeField] private Vector3 direction;
+    [SerializeField] private Vector2 startTouch;
     [SerializeField] private float speed = 5f;
-
-    public float Speed
-    {
-        get => speed;
-        set => speed = value;
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
     }
 
     private void HandleMovement()
     {
-        if(Input.GetMouseButtonDown(0))
+        rbPlayer.velocity = new Vector3(fJoyStick.Horizontal * speed, rbPlayer.velocity.y, fJoyStick.Vertical * speed);
+
+        if(fJoyStick.Horizontal != 0 || fJoyStick.Vertical != 0)
         {
-
-        }
-
-        if(Input.GetMouseButtonUp(0))
-        {
-
+            transform.rotation = Quaternion.LookRotation(new Vector3(fJoyStick.Horizontal, 0, fJoyStick.Vertical));
         }
     }
 }
