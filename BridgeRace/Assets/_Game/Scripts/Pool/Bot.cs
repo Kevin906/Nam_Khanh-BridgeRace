cb3773d6f2b912b805a6ddf2902bb1b5d41705bf
Khanh <Kn6267658@gmail.com>
2025-10-10 17:09:24 +0700

    ~ Bot

--- BridgeRace/Assets/_Game/Scripts/Pool/Bot.cs
+++ BridgeRace/Assets/_Game/Scripts/Pool/Bot.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Bot : GameUnit
 {
     [SerializeField] private Renderer rdBot;
+    [SerializeField] private Renderer rbBotBrick;
     public EColorType colorType = EColorType.Default;
 
     public void SetColor(Material mat, EColorType type)
     {
-        if (rdBot != null && mat != null)
+        if (rbBotBrick != null && rdBot != null && mat != null)
         {
             rdBot.material = mat;
+            rbBotBrick.material = mat;
             colorType = type;
         }
     }
