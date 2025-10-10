9e7aa873a5e567e5b85d0d5ebb351ba84385e10c
Khanh <Kn6267658@gmail.com>
2025-10-10 17:08:41 +0700

    ~ Player

--- BridgeRace/Assets/_Game/Scripts/Player.cs
+++ BridgeRace/Assets/_Game/Scripts/Player.cs
@@ -5,30 +5,58 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private GameObject playerBrickPrefabs;
-    [SerializeField] private Rigidbody rbPlayer;
-    [SerializeField] private Transform tfPlayerBrick;
-    [SerializeField] private FixedJoystick fJoyStick;
-    [SerializeField] private float speed = 5f;
+	[Header("Refs")]
+	[SerializeField] private GameObject playerBrickPrefabs;
+	[SerializeField] private Rigidbody rbPlayer;
+	[SerializeField] private Transform tfPlayerBrick;
+	[SerializeField] private FixedJoystick fJoyStick;
+	[SerializeField] private MeshRenderer mrPlayer;
+	[SerializeField] private MeshRenderer mrPlayerBrick;
 
-    // Update is called once per frame
-    void Update()
-    {   
-        HandleMovement();
-    }
+	[Header("Movement")]
+	[SerializeField] private float speed = 5f;
 
-    private void HandleMovement()
-    {
-        rbPlayer.velocity = new Vector3(fJoyStick.Horizontal * speed, rbPlayer.velocity.y, fJoyStick.Vertical * speed);
+	[Header("Gameplay")]
+	public EColorType colorType;
 
-        if(fJoyStick.Horizontal != 0 || fJoyStick.Vertical != 0)
-        {
-            transform.rotation = Quaternion.LookRotation(new Vector3(fJoyStick.Horizontal, 0, fJoyStick.Vertical));
-        }
-    }
+	void Update()
+	{
+		HandleMovement();
+	}
 
-    private void Addbrick(Collider col)
-    {
+	private void HandleMovement()
+	{
+		rbPlayer.velocity = new Vector3(fJoyStick.Horizontal * speed, rbPlayer.velocity.y, fJoyStick.Vertical * speed);
 
-    }
+		if (fJoyStick.Horizontal != 0 || fJoyStick.Vertical != 0)
+		{
+			transform.rotation = Quaternion.LookRotation(new Vector3(fJoyStick.Horizontal, 0, fJoyStick.Vertical));
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Addbrick(other);
+	}
+
+	private void Addbrick(Collider col)
+	{
+		//if (col == null) return;
+
+		//Brick brick = col.GetComponentInParent<Brick>();
+		//if (brick != null)
+		//{
+		//	HBPool.Despawn(brick);
+		//}
+	}
+	public void SetColor(Material mat, EColorType type)
+	{
+		colorType = type;
+		if (mrPlayer != null && mrPlayerBrick != null && mat != null)
+		{
+			mrPlayer.material = mat;
+			mrPlayerBrick.material = mat;
+			colorType = type;
+		}
+	}
 }
